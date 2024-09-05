532d8fed646ed9d55c93092b6eff0968c0742f20
Andy-Siphaxay <s3987059@student.rmit.edu.au>
2024-09-06 02:31:41 +1000

    Article class for RSS

---
+++ src/main/java/au/edu/rmit/sept/webapp/model/ArticleRss.java
@@ -0,0 +1,22 @@
+package au.edu.rmit.sept.webapp.model;
+
+public class ArticleRss {
+    private String title;
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
