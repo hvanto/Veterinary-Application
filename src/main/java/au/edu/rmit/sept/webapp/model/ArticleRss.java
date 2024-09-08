161394ac9e855df90d99504cdb161106607fa22b
Andy-Siphaxay <s3987059@student.rmit.edu.au>
2024-09-08 18:30:56 +1000

    Add new attributes

--- src/main/java/au/edu/rmit/sept/webapp/model/ArticleRss.java
+++ src/main/java/au/edu/rmit/sept/webapp/model/ArticleRss.java
@@ -1,8 +1,14 @@
 package au.edu.rmit.sept.webapp.model;
 
+import java.util.Date;
+
 public class ArticleRss {
     private String title;
     private String link;
+    private String description;
+    private String author;
+    private Date publishedDate;
+    private String imageUrl;
 
     public String getLink() {
         return link;
@@ -18,5 +24,37 @@ public class ArticleRss {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
