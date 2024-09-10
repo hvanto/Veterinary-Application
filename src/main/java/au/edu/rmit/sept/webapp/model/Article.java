015ec300059e74c373daf69113be04a387854d61
Andy-Siphaxay <s3987059@student.rmit.edu.au>
2024-09-10 17:33:32 +1000

    Modify Article class

--- src/main/java/au/edu/rmit/sept/webapp/model/Article.java
+++ src/main/java/au/edu/rmit/sept/webapp/model/Article.java
@@ -1,10 +1,8 @@
 package au.edu.rmit.sept.webapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "articles")
@@ -13,18 +11,23 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    private String title;
-    private String content;
 
-    // Getters and setters
-    
-    public Long getId() {
-        return id;
+    private String title;
+    private String link;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String author;
+    private Date publishedDate;
+    private String imageUrl;
+
+    public String getLink() {
+        return link;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setLink(String link) {
+        this.link = link;
     }
 
     public String getTitle() {
@@ -35,12 +38,36 @@ public class Article {
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
+    public String getDescription() {
+        return description;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
 
