df60128efc98920f78e431a291be71a7e6d00ed6
Henry van Toledo <s3849054@student.rmit.edu.au>
2024-09-11 19:55:50 +1000

    Templates for user creation

--- src/main/java/au/edu/rmit/sept/webapp/controller/UserController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controller/UserController.java
@@ -1,7 +1,23 @@
 package au.edu.rmit.sept.webapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+//IMPORTS OFR USER SERVICE AND USER REPOSITORY
+
 // User Model
 import au.edu.rmit.sept.webapp.model.User;
 
 public class UserController {
+
+    // User login
+    @PostMapping("/login")
+    public String login(User user) {
+        // Login logic
+
+        // Once user info is recieved, user the user service to authenticate and send to the user table
+
+        return "redirect:/home";
+    }
 }
