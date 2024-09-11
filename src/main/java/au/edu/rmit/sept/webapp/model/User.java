cb009dc65586b34ffb97e41d66dacbcf17ecf43f
ashmit-sachan <ashmit666@gmail.com>
2024-09-11 10:31:06 +1000

    Added USER Entity

--- src/main/java/au/edu/rmit/sept/webapp/model/User.java
+++ src/main/java/au/edu/rmit/sept/webapp/model/User.java
@@ -12,88 +12,26 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
-    private String lastName;
+    private String first_name;
+    private String last_name;
 
     @Column(unique = true)
     private String email;
+    private String contact;
+    private String image;
 
     private String password;
 
-    @ElementCollection
-    @CollectionTable(name = "connected_accounts", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "account")
-    private List<String> connectedAccounts;
-
-    @Column(name = "created_on", updatable = false)
+    @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private Date createdOn;
+    private Date created_on;
 
     private boolean deleted;
 
     @PrePersist
     protected void onCreate() {
-        createdOn = new Date();
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<String> getConnectedAccounts() {
-        return connectedAccounts;
-    }
-
-    public void setConnectedAccounts(List<String> connectedAccounts) {
-        this.connectedAccounts = connectedAccounts;
-    }
-
-    public Date getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
+        image = "default_profile.png";
+        created_on = new Date();
+        deleted = false;
     }
 }
