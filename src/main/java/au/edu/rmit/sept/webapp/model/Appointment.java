812f3c2731980b406f018fd614c89a001b528a23
ashmit-sachan <ashmit666@gmail.com>
2024-09-12 16:39:51 +1000

    Entities modified

--- src/main/java/au/edu/rmit/sept/webapp/model/Appointment.java
+++ src/main/java/au/edu/rmit/sept/webapp/model/Appointment.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @implNote    1.Pet column will be added to this Entity after the pet table has been created.
+ */
 @Entity
 @Table(name = "appointment")
 public class Appointment {
@@ -29,5 +32,70 @@ public class Appointment {
     @Temporal(TemporalType.TIMESTAMP)
     private Date UpdatedOn;
 
+    @PrePersist
+    protected void onCreate() {
+        CreatedOn = new Date();
+        UpdatedOn = new Date();
+    }
 
+    @PreUpdate
+    protected void onUpdate() {
+        UpdatedOn = new Date();
+    }
+
+    // Constructors
+    public Appointment() {
+    }
+
+    public Appointment(Date dateTime, String notes, User user, Veterinarian veterinarian) {
+        DateTime = dateTime;
+        Notes = notes;
+        this.user = user;
+        this.veterinarian = veterinarian;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return Id;
+    }
+
+    public Date getCreatedOn() {
+        return CreatedOn;
+    }
+
+    public Date getUpdatedOn() {
+        return UpdatedOn;
+    }
+
+    public Date getDateTime() {
+        return DateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        DateTime = dateTime;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
 }
--- src/main/java/au/edu/rmit/sept/webapp/model/Clinic.java
+++ src/main/java/au/edu/rmit/sept/webapp/model/Clinic.java
@@ -3,12 +3,17 @@ package au.edu.rmit.sept.webapp.model;
 import au.edu.rmit.sept.webapp.enums.Weekday;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "clinic")
 public class Clinic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @OneToMany(mappedBy = "veterinarian")
+    private List<Veterinarian> veterinarians;
 
     private String Name;
     private String Email;
