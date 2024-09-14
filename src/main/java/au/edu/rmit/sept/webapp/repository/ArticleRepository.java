63406fd80567182e52091c1df08e882ec5f7dcf5
s3971187 <s3971187@student.rmit.edu.au>
2024-09-14 19:56:14 +1000

    Added function to find article by link

--- src/main/java/au/edu/rmit/sept/webapp/repository/ArticleRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repository/ArticleRepository.java
@@ -2,7 +2,11 @@ package au.edu.rmit.sept.webapp.repository;
 
 
 import au.edu.rmit.sept.webapp.model.Article;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Optional<Article> findByLink(String link);
 }
--- src/main/java/au/edu/rmit/sept/webapp/service/ArticleService.java
+++ src/main/java/au/edu/rmit/sept/webapp/service/ArticleService.java
@@ -29,6 +29,10 @@ public class ArticleService {
         return repository.findById(id);
     }
 
+    public Optional<Article> getArticleByLink(String link) {
+        return repository.findByLink(link);
+    }
+
     public Article saveArticle(Article article) {
         return repository.save(article);
     }
@@ -39,6 +43,11 @@ public class ArticleService {
 
     public List<Article> getAllArticles() {
         return repository.findAll();
+    }
+
+    // Check if an article exists by link
+    public boolean articleExists(String link) {
+        return repository.findByLink(link).isPresent();
     }
 
     // Pagination
