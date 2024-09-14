32c9efdc4c6617d88c21a50ea49dc26f0b69c57c
s3971187 <s3971187@student.rmit.edu.au>
2024-09-14 20:00:17 +1000

    Implemented function to find first user in table for testing

--- src/main/java/au/edu/rmit/sept/webapp/repository/UserRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repository/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Find first user in table
+    Optional<User> findFirstByOrderByIdAsc();
 
     // Find a user by email
     Optional<User> findByEmail(String email);
