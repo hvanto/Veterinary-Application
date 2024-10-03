014c91baa126b9f9eeccec79e923b8b26b0cab33
s3971187 <s3971187@student.rmit.edu.au>
2024-10-03 11:44:17 +1000

    Removed unneccessary dependencies

--- src/main/java/au/edu/rmit/sept/webapp/controller/ArticleController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controller/ArticleController.java
@@ -1,7 +1,5 @@
 package au.edu.rmit.sept.webapp.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import au.edu.rmit.sept.webapp.model.Article;
 import au.edu.rmit.sept.webapp.model.Bookmark;
@@ -32,7 +27,6 @@ import au.edu.rmit.sept.webapp.service.UserService;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
