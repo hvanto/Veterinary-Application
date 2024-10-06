70dd552f908ceb7a0d24ae0be098a17e653e98e3
Andy-Siphaxay <s3987059@student.rmit.edu.au>
2024-10-06 17:57:21 +1100

    Create faq controller

---
+++ src/main/java/au/edu/rmit/sept/webapp/controller/FAQController.java
@@ -0,0 +1,36 @@
+package au.edu.rmit.sept.webapp.controller;
+
+import au.edu.rmit.sept.webapp.model.FAQ;
+import au.edu.rmit.sept.webapp.repository.FAQRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class FAQController {
+
+    @Autowired
+    private FAQRepository faqRepository;
+
+    @GetMapping("/faq/{category}")
+    public String getFaqById(@PathVariable("category") String category, Model model) {
+        try {
+            List<FAQ> faqs = faqRepository.findByCategory(category);
+
+            model.addAttribute("faqs", faqs);
+            model.addAttribute("category", category);
+            model.addAttribute("isEmpty", faqs.isEmpty());
+
+        } catch (Exception e) {
+            model.addAttribute("error", "An error occurred while retrieving FAQs.");
+        }
+
+        model.addAttribute("content", "faq");
+        return "index";
+    }
+
+}
