84276e31cd40bf98910611e88077832692bc28a7
s3971187 <s3971187@student.rmit.edu.au>
2024-10-10 07:25:40 +1100

    Implemented repository method to find pet by vet id

--- src/main/java/au/edu/rmit/sept/webapp/repository/MedicalHistoryRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repository/MedicalHistoryRepository.java
@@ -1,7 +1,11 @@
 package au.edu.rmit.sept.webapp.repository;
 
 import au.edu.rmit.sept.webapp.model.MedicalHistory;
+import au.edu.rmit.sept.webapp.model.Pet;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +17,9 @@ public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, 
 
     // Fetch medical history by the prescription's id
     List<MedicalHistory> findByPrescriptionId(Long prescriptionId);
+
+    @Query(value = "SELECT DISTINCT p FROM Pet p "
+            + "JOIN medical_history mh ON p.id = mh.pet_id "
+            + "WHERE mh.veterinarian_id = :vet_id", nativeQuery = true)
+    List<Pet> findPetByVeterinarianId(@Param("vet_id") Long veterinarianId);
 }
